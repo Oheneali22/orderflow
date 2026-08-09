290798e9747a8639c280ef0949b4d32e33776473
Ali <babagaddafi1@gmail.com>
2026-08-09 13:34:12 -0400

    Fix immutable GitHub OIDC trust

--- terraform/delivery/main.tf
+++ terraform/delivery/main.tf
@@ -71,7 +71,7 @@ data "aws_iam_policy_document" "github_trust" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/main"]
+      values   = ["${var.github_oidc_subject_prefix}:ref:refs/heads/main"]
     }
   }
 }
